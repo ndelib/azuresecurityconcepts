69b101dc569d5ee02c85744112b881c2483f54c9
ndelib <69071207+ndelib@users.noreply.github.com>
2023-09-24 00:25:40 +1000

    fix broken a href

--- src/lib/components/oauth2_and_oidc/TopicOAuth2OIDC.svelte
+++ src/lib/components/oauth2_and_oidc/TopicOAuth2OIDC.svelte
@@ -7,8 +7,8 @@
 
 <div class="hero bg-base-200 rounded-box" style="height: calc(100vh - 164px);">
     <div class="flex justify-between w-full transform -translate-y-1/2 left-5 right-5 top-1/2 pr-4 pl-4">
-      <a href="#slide4" class="btn btn-circle btn-outline">❮</a> 
-      <a href="#slide2" class="btn btn-circle btn-outline">❯</a>
+      <a href="#" class="btn btn-circle btn-outline">❮</a> 
+      <a href="#" class="btn btn-circle btn-outline">❯</a>
     </div>
     <div class="hero-content text-center">
       <div class="max-w-md">
