52dca274272f4f3328d0198663d216f642e0b3e5
ndelib <69071207+ndelib@users.noreply.github.com>
2023-09-24 00:20:56 +1000

    started to add skeleton slide outline

--- src/lib/components/SlideContent.svelte
+++ src/lib/components/SlideContent.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
+    import TopicOAuth2OIDC from "$lib/components/oauth2_and_oidc/TopicOAuth2OIDC.svelte"
+    export let curSlide
 </script>
 
 <!-- <div class="flex justify-center w-full" style="height: calc(100vh - 164px);">
     Authentication and Authorization is really cool man.
 </div> -->
 
-<div class="hero bg-base-200 rounded-box" style="height: calc(100vh - 164px);">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
+{#if curSlide == "oauth2_and_oidc"}
+    <TopicOAuth2OIDC/>
+{:else}
+    <div class="hero bg-base-200 rounded-box" style="height: calc(100vh - 164px);">
+        <div class="hero-content text-center">
+        <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Hello there</h1>
+            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button class="btn btn-primary">Get Started</button>
+        </div>
+        </div>
     </div>
-</div>
+{/if}
 
 <style>
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,54 @@
 <script lang="ts">
-	import CategorySidebar from "$lib/components/CategorySidebar.svelte";
-	import SlideContent from "$lib/components/SlideContent.svelte";
+    import CategorySidebar from "$lib/components/CategorySidebar.svelte";
+    import SlideContent from "$lib/components/SlideContent.svelte";
 </script>
 
-<div class="navbar bg-base-100">
-	<a class="btn btn-ghost normal-case text-xl">Azure Security Concepts</a>
+<div class="navbar bg-base-100 space-x-12">
+    <a class="btn btn-ghost normal-case text-xl">Azure Security Concepts</a>
+    <div class="text-lg breadcrumbs">
+        <ul>
+            <li><a>Authentication & Authorization</a></li> 
+            <li><a>OAuth2.0 & Open ID Connect in Azure Entra ID</a></li> 
+        </ul>
+    </div>
 </div>
 
 <!-- Main content goes here -->
 <div class="flex space-x-4 justify-center w-full">
 
-	<div class="flex flex-col justify-center w-full pl-4">
-		<SlideContent />
-		<progress class="progress progress-accent w-full -top-2" value="30" max="100"></progress>
-	</div>
+    <div class="flex flex-col justify-center w-full pl-4">
+        <SlideContent curSlide="oauth2_and_oidc"/>
+        <progress class="progress progress-accent w-full -top-2" value="30" max="100"></progress>
+    </div>
 
-	<div class="flex pr-4">
-		<ul class="menu bg-base-200 w-56 rounded-box" style="height: calc(100vh - 164px);">
-			<li>
-			<h2 class="menu-title">Title</h2>
-			<ul>
-				<li><a>Item 1</a></li>
-				<li><a>Item 2</a></li>
-				<li><a>Item 3</a></li>
-			</ul>
-			</li>
-		</ul>
-	</div>
+    <div class="flex pr-4">
+        <ul class="menu bg-base-200 w-96 rounded-box" style="height: calc(100vh - 164px);">
+            <li>
+            <h2 class="menu-title">Table of Contents</h2>
+            <ul>
+                <li><a>What you'll learn here</a></li>
+                <li><a>Why are OAuth2.0 & OIDC important?</a></li>
+                <li><a>Intro 1: Authentication vs Authorization?</a></li>
+                <li><a>Intro 2: History</a></li>
+                <li><a>Problem OAuth2.0 Solves</a></li>
+                <li><a>High Level Overview</a></li>
+                <li><a>High Level Overview With Jargon</a></li>
+            </ul>
+            </li>
+        </ul>
+    </div>
 </div>
 
 <div class="drawer">
-	<input id="my-drawer" type="checkbox" class="drawer-toggle" />
-	<div class="drawer-side">
-		<label for="my-drawer" class="drawer-overlay" />
-		<CategorySidebar />
-	</div>
+    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-side">
+        <label for="my-drawer" class="drawer-overlay" />
+        <CategorySidebar />
+    </div>
 </div>
 
 <div class="flex justify-center w-full pt-5">
-	<label for="my-drawer" class="btn btn-primary drawer-button">Show All Concepts</label>
+    <label for="my-drawer" class="btn btn-primary drawer-button">Show All Concepts</label>
 </div>
 
 <style>
